f4ee08654c8472fbbe9d7077a9280cd4bfced7be
Bhushan <bhushanmusale795@outlook.com>
2024-06-29 21:25:27 +0530

    hi

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,78 +1,42 @@
 package com.driver;
 
-
-
+// Main class containing the Product class
 public class Main {
 
-    // Nested class Product
+    // Inner class Product
     public static class Product {
-        // Member variables
-        private int productId;
-        private String productName;
-        private double price;
 
-        // Constructor
-        public Product(int productId, String productName, double price) {
-            this.productId = productId;
-            this.productName = productName;
-            this.price = price;
+        // Method to calculate product of two integers
+        public int product(int x, int y) {
+            return x * y;
         }
 
-        // Getter and Setter methods
-        public int getProductId() {
-            return productId;
+        // Overloaded method to calculate product of three integers
+        public int product(int x, int y, int z) {
+            return x * y * z;
         }
 
-        public void setProductId(int productId) {
-            this.productId = productId;
-        }
-
-        public String getProductName() {
-            return productName;
-        }
-
-        public void setProductName(String productName) {
-            this.productName = productName;
-        }
-
-        public double getPrice() {
-            return price;
-        }
-
-        public void setPrice(double price) {
-            this.price = price;
+        // Overloaded method to calculate product of two doubles
+        public double product(double x, double y) {
+            return x * y;
         }
     }
 
-    // Method to calculate product of two integers
-    public int product(int x, int y) {
-        return x * y;
-    }
-
-    // Overloaded method to calculate product of two doubles
-    public double product(double x, double y) {
-        return x * y;
-    }
-
-    // Main method
+    // Main method to demonstrate polymorphism
     public static void main(String[] args) {
-        // Creating an instance of Product called p
-        Product p = new Product(1, "Laptop", 999.99);
+        // Task 2: Create an object of Product
+        Product p = new Product();
 
-        // Using getter methods to retrieve information
-        System.out.println("Product ID: " + p.getProductId());
-        System.out.println("Product Name: " + p.getProductName());
-        System.out.println("Product Price: $" + p.getPrice());
+        // Task 3: Call method product(int x, int y) from Product class object p
+        int result1 = p.product(5, 7);
+        System.out.println("Product of 5 and 7 (integers): " + result1);
 
-        // Calling the product methods using p
-        Main main = new Main();
+        // Task 4: Call method product(int x, int y, int z) from Product class object p
+        int result2 = p.product(2, 3, 4);
+        System.out.println("Product of 2, 3, and 4 (integers): " + result2);
 
-        // Call the product method for two integers
-        int result1 = main.product(5, 7);
-        System.out.println("Product of 5 and 7 (integers) is: " + result1);
-
-        // Call the overloaded product method for two doubles
-        double result2 = main.product(2.5, 3.5);
-        System.out.println("Product of 2.5 and 3.5 (doubles) is: " + result2);
+        // Task 5: Call method product(double x, double y) from Product class object p
+        double result3 = p.product(2.5, 3.5);
+        System.out.println("Product of 2.5 and 3.5 (doubles): " + result3);
     }
 }
